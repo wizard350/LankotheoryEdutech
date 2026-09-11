6ff4a35e791b37810fdfa0a8ad5f246e5e7c66d0
wizard350 <wizardsmart478@gmail.com>
2026-09-11 13:22:39 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -296,3 +296,17 @@ if (menuToggle && navbar) {
 
 }
 });
+document.addEventListener("DOMContentLoaded", function () {
+
+    const hamburger = document.getElementById("hamburger");
+    const navMenu = document.getElementById("navMenu");
+
+    if (hamburger && navMenu) {
+
+        hamburger.addEventListener("click", function () {
+            navMenu.classList.toggle("active");
+        });
+
+    }
+
+});
